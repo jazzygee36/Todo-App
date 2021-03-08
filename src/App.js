da3d8e61538cfab8d2cc01d1b157172d3f950df7
Sam <jazzygee36@gmail.com>
2021-03-08 12:02:31 +0000

    Initial commit :fire: :fire: :fire:

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import TodoList from "./Components/TodoList.jsx";
+
+
+
+function App() {
+  return (
+    <div className="Todo-app">
+
+      <TodoList />
+      
+     
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+const TodoList = () => {
+const [todos, setTodos] = useState([]);
+
+const addTodo = todo => {
+    if(!todo.text || /^\s*$/.test(todo.text)) {
+        return;
+    }
+
+    const newTodos = [todo, ...todos]
+
+    setTodos(newTodos);
+    
+};
+
+const updateTodo = (todoId, newValue) => {
+    if(!newValue.text || /^\s*$/.test(newValue.text)) {
+        return;
+    }
+
+    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item))
+    );
+
+};
+
+
+
+
+const removeTodo = id => {
+    const removeArr = [...todos].filter(todo => todo.id !== id )
+
+    setTodos(removeArr);
+};
+
+
+
+
+
+const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+        if (todo.id === id) {
+            todo.isComplete = !todo.isComplete
+        }
+        return todo
+    })
+    setTodos(updatedTodos);
+}
+
+    return (
+        <div id="Todos">
+            <h1>Plan your day with our App</h1>
+            <TodoForm onSubmit={addTodo} />
+            <Todo 
+            todos={todos}
+            completeTodo={completeTodo} removeTodo=
+            {removeTodo} updateTodo={updateTodo}
+            /> 
+        </div>
+    )
+}
+
+export default TodoList
